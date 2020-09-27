a5c590db930fedc43e9344d599afb1707306d75a
Enes Tüfekçi <enesxtufekci@gmail.com>
2020-09-27 16:53:53 +0200

    prisma init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
+import { AppContext, AppInitialProps } from "next/app";
 import "../styles/main.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppInitialProps & AppContext) {
   return <Component {...pageProps} />;
 }
 
